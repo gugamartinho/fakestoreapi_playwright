1b0cef0e09b156146c02c5f7ab152b86b3df3bfc
David Martinho <gugamartinho@gmail.com>
2026-04-22 14:49:48 +0100

    removido paralelismo no CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  fullyParallel: true,
+  fullyParallel: process.env.CI ? false : true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 2 : undefined,
+  retries: process.env.CI ? 1 : 0,
+  workers: process.env.CI ? 1 : undefined,
   reporter: [
     ['html'],
     ['list']
